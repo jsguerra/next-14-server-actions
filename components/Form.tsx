9ba589d1d59fd32c11c8d879ffce07815ca2a434
jsguerra <jose@gcostudios.com>
2023-11-03 10:01:56 +0000

    updated

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+"use client"
+
+import { addTodo } from "@/actions/actions";
+import React from "react";
 
 export default function Form() {
   return (
-    <div>Form</div>
-  )
+    <form action={addTodo} className="flex flex-col w-[300px] my-16">
+      <input
+        type="text"
+        name="content"
+        className="px-4 py-2 mb-3"
+        placeholder="Write your todo..."
+        required
+      />
+      <button className="bg-blue-500 rounded px-4 py-2 text-white font-semibold">
+        Add
+      </button>
+    </form>
+  );
 }
