4978e61524b908c2da01e5b681f11b5e0a27a6db
Jose Guerra <me@joseguerra.uk>
2023-11-08 21:12:50 +0000

    added upload function

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,6 +1,9 @@
 "use server";
 
 import prisma from "@/app/lib/prisma";
+import fs from "fs";
+import { writeFile } from "fs/promises";
+import { join } from "path";
 import { revalidatePath } from "next/cache";
 
 export const addTodo = async (formData: FormData) => {
@@ -11,10 +14,46 @@ export const addTodo = async (formData: FormData) => {
       data: {
         content: content as string,
       },
-    }); 
+    });
   } catch (error) {
     console.error(error);
   }
 
   revalidatePath("/todos");
 };
+
+export async function upload(data: FormData) {
+  const name = data.get("name");
+  const authorPath = data.get("path");
+  const thumbnail = data.get("thumbnail");
+
+  await prisma.author.create({
+    data: {
+      name: name as string,
+      path: authorPath as string,
+      thumbnail: thumbnail as string,
+    },
+  });
+
+  const file: File | null = data.get("imageUpload") as unknown as File;
+  if (!file) {
+    throw new Error("No file uploaded");
+  }
+
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  const myPath = "public/collections";
+  const firstLetter = name?.slice(0, 1);
+  let destination = `${myPath}/${firstLetter}`;
+
+  if (!fs.existsSync(destination)) {
+    fs.mkdirSync(destination, { recursive: true });
+  }
+
+  const path = join("./", destination, file.name);
+  await writeFile(path, buffer);
+  console.log(`open ${path} to see the uploaded file`);
+
+  return { success: true };
+}
